f8bf8ddfe8e5a9f3afc69a3a4111403744064c8f
janhavihirwe <janhavihirwe@gmail.com>
2024-11-28 14:39:22 +0530

    SignUp functionality added

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -6,6 +6,21 @@ require("dotenv").config()
 
 const router = express.Router();
 
+router.post('/signup', async (req, res) => {
+    const { username, email, password, role } = req.body;
+    try {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) return res.status(400).json({ message: 'User already exists!' });
+  
+      const user = new User({ usernamename, email, password, role });
+      await user.save();
+      res.status(201).json({ message: 'User registered successfully!' });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error creating user' });
+    }
+  });
+
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
