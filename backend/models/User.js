494d53339cd94e346fd3182e31d73bc09b8c9f6e
janhavihirwe <janhavihirwe@gmail.com>
2024-11-28 14:52:51 +0530

    Signup functionality added

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,10 +2,16 @@ const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
   username: { type: String, required: true },
-  email:{type:String,required:true},
+  email:{type:String,required:true,unique: true},
   password: { type: String, required: true },
   role: { type: String, enum: ["admin", "user"], default: "user" },
 }, { versionKey: false });
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  });
 
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -12,7 +12,7 @@ router.post('/signup', async (req, res) => {
       const existingUser = await User.findOne({ email });
       if (existingUser) return res.status(400).json({ message: 'User already exists!' });
   
-      const user = new User({ usernamename, email, password, role });
+      const user = new User({ username, email, password, role });
       await user.save();
       res.status(201).json({ message: 'User registered successfully!' });
     } catch (err) {
